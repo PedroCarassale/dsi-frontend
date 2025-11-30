a9c2c22c0b78260c984f15de4a8a196567365a04
Pedro Carassale <pedrocarassale@gmail.com>
2025-11-30 12:54:17 -0300

    add login to router

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MemoriasAnuales from "./pages/memorias-anuales/MemoriasAnuales";
 import RegistrosPatentes from "./pages/registros-patentes/RegistrosPatentes";
 import TrabajosPublicados from "./pages/trabajos-publicados/index";
 import AppLayout from "../components/AppLayout";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <AppLayout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/memorias-anuales" element={<MemoriasAnuales />} />
           <Route path="/registros-patentes" element={<RegistrosPatentes />} />
           <Route path="/trabajos-publicados" element={<TrabajosPublicados />} />
