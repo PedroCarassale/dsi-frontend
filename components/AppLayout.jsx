0579413b357c1bb1204c490a22c36e1384ab41f2
PedroCarassale <pedrocarassale@gmail.com>
2025-12-03 18:05:02 -0300

    comments deleted

--- components/AppLayout.jsx
+++ components/AppLayout.jsx
@@ -3,17 +3,15 @@ import Layout from "./Layout";
 
 function AppLayout({ children }) {
   const location = useLocation();
-  
-  // Páginas que NO usan el header (como login)
-  const noLayoutPages = ['/login'];
-  
+
+  const noLayoutPages = ["/login"];
+
   const shouldShowLayout = !noLayoutPages.includes(location.pathname);
-  
+
   if (shouldShowLayout) {
     return <Layout>{children}</Layout>;
   }
-  
-  // Para páginas sin layout (como Login)
+
   return <div className="min-h-screen">{children}</div>;
 }
 
